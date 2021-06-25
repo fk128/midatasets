31ef5e926ed5eb8311f145ec7b627fe0104def52
Fahdi Kanavati <fkanavati@gmail.com>
2021-06-25 10:21:23 +0100

    Bump version: 0.3.18 → 0.3.19

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as requirements_file:
 
 
 setup(name='midatasets',
-      version='0.3.18',
+      version='0.3.19',
       description='Medical Image Dataset tools',
       author='F. K.',
       keywords='medical ',
