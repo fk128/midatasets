f78d3e8c15a8a3f7249d39a891f6a8eeb7935595
fk128 <fkanavati@gmail.com>
2021-09-15 11:49:21 +0100

    Add tests

---
+++ tests/test_dataset_storage.py
@@ -0,0 +1,124 @@
+from pathlib import Path
+
+import boto3
+from midatasets import backends
+from midatasets.MIReader import MIReader
+from moto import mock_s3
+
+
+def test_local_backend_sublabels(tmpdir):
+    p = Path(tmpdir)
+    labels = ["l1", "l2"]
+    for dataset_name in ["foo", "bar"]:
+        for l in labels:
+            (p / dataset_name / "labelmaps" / l / "native").mkdir(
+                exist_ok=True, parents=True
+            )
+        (p / dataset_name / "images" / "native").mkdir(exist_ok=True, parents=True)
+        for i in range(10):
+            for l in labels:
+                (
+                    p
+                    / dataset_name
+                    / "labelmaps"
+                    / l
+                    / "native"
+                    / f"image_{i}_seg.nii.gz"
+                ).touch()
+            (p / dataset_name / "images" / "native" / f"image_{i}.nii.gz").touch()
+
+    dataset = MIReader(
+        dir_path=str(Path(tmpdir) / "foo"), spacing=0, remote_backend=None
+    )
+
+    assert list(dataset.dataframe.columns) == [
+        "image_path",
+        "labelmap-l2_path",
+        "labelmap-l1_path",
+    ]
+    assert len(dataset.dataframe) == 10
+
+
+def test_local_backend(tmpdir):
+    p = Path(tmpdir)
+    for dataset_name in ["foo", "bar"]:
+        (p / dataset_name / "labelmaps" / "native").mkdir(exist_ok=True, parents=True)
+        (p / dataset_name / "images" / "native").mkdir(exist_ok=True, parents=True)
+        for i in range(10):
+            (
+                p / dataset_name / "labelmaps" / "native" / f"image_{i}_seg.nii.gz"
+            ).touch()
+            (p / dataset_name / "images" / "native" / f"image_{i}.nii.gz").touch()
+
+    dataset = MIReader(
+        dir_path=str(Path(tmpdir) / "foo"), spacing=0, remote_backend=None
+    )
+    assert len(dataset.dataframe) == 10
+
+
+def test_local_backend_duplicate(tmpdir):
+    p = Path(tmpdir)
+    for dataset_name in ["foo", "bar"]:
+        (p / dataset_name / "labelmaps" / "native").mkdir(exist_ok=True, parents=True)
+        (p / dataset_name / "images" / "native").mkdir(exist_ok=True, parents=True)
+        for i in range(10):
+            (
+                p / dataset_name / "labelmaps" / "native" / f"image_{i}_seg.nii.gz"
+            ).touch()
+            (p / dataset_name / "images" / "native" / f"image_{i}.nii.gz").touch()
+        for i in range(10):
+            (
+                p / dataset_name / "labelmaps" / "native" / f"image_{i}_seg.nii.gz"
+            ).touch()
+            (p / dataset_name / "images" / "native" / f"image_{i}_2.nii.gz").touch()
+
+    dataset = MIReader(
+        dir_path=str(Path(tmpdir) / "foo"), spacing=0, remote_backend=None
+    )
+    assert len(dataset.dataframe) == 2 * 10
+
+
+@mock_s3
+def test_s3_backend(tmpdir):
+    conn = boto3.resource("s3", region_name="us-east-1")
+    conn.create_bucket(Bucket="mybucket")
+    s3 = boto3.client("s3", region_name="us-east-1")
+    datasets = {}
+    for dataset_name in ["foo", "bar"]:
+        datasets[dataset_name] = f"datasets/{dataset_name}/"
+        for i in range(10):
+            key = f"datasets/{dataset_name}/labelmaps/native/img_{i}.nii.gz"
+            s3.put_object(Bucket="mybucket", Key=key, Body="")
+            key = f"datasets/{dataset_name}/images/native/img_{i}.nii.gz"
+            s3.put_object(Bucket="mybucket", Key=key, Body="")
+
+    backend = backends.S3Backend(bucket="mybucket", prefix="datasets/")
+
+    assert datasets == backend.list_datasets()
+
+    backend.download("foo", tmpdir, max_images=10)
+    assert len(list(Path(str(tmpdir)).rglob("*.gz"))) == 2 * 10
+
+
+@mock_s3
+def test_s3_backend_sublabel(tmpdir):
+    conn = boto3.resource("s3", region_name="us-east-1")
+    conn.create_bucket(Bucket="mybucket")
+    s3 = boto3.client("s3", region_name="us-east-1")
+    datasets = {}
+    labels = ["l1", "l2", "l3"]
+    for dataset_name in ["foo", "bar"]:
+        datasets[dataset_name] = f"datasets/{dataset_name}/"
+        for spacing in ["native", "subsampled1mm"]:
+            for i in range(10):
+                for l in labels:
+                    key = f"datasets/{dataset_name}/labelmaps/{l}/{spacing}/img_{i}_seg.nii.gz"
+                    s3.put_object(Bucket="mybucket", Key=key, Body="")
+                key = f"datasets/{dataset_name}/images/{spacing}/img_{i}.nii.gz"
+                s3.put_object(Bucket="mybucket", Key=key, Body="")
+
+    backend = backends.S3Backend(bucket="mybucket", prefix="datasets/")
+
+    assert datasets == backend.list_datasets()
+    backend.download("foo", f"{tmpdir}/foo", max_images=10)
+    assert len(list(Path(str(tmpdir)).rglob("*.gz"))) == 4 * 10
---
+++ tests/test_db.py
@@ -0,0 +1,56 @@
+import os
+
+import pytest
+from midatasets.databases import (
+    MIDatasetDBBaseYaml,
+    MIDatasetModel,
+    CompositeDB, MIDatasetDBBaseMongoDb,
+)
+from midatasets.datasets import MIDatasets
+
+
+def test_datasets(tmp_path):
+    os.environ['MIDATASETS_YAML_PATH'] = f"{tmp_path}/midatasets.yaml"
+    datasets = MIDatasets(db="yaml")
+    datasets.create(
+        MIDatasetModel(name="foo", aws_s3_prefix="bar", aws_s3_bucket="f")
+    )
+    assert len(datasets.get_info_all()) == 1
+
+
+@pytest.mark.skip(msg="TODO: mock mongo")
+def test_mongodb():
+    db = MIDatasetDBBaseMongoDb()
+    print(db.find_all())
+
+
+def test_yaml_crud(tmp_path):
+    db = MIDatasetDBBaseYaml(path=f"/{tmp_path}/midatasets.yaml")
+    m = MIDatasetModel(name="foo", aws_s3_bucket="v", aws_s3_prefix="s")
+
+    db.create(m)
+
+    res = db.find({"name": "foo"})
+    assert res != None
+    m2 = MIDatasetModel(name="bar", aws_s3_bucket="v", aws_s3_prefix="s")
+    db.update({"name": "foo"}, m2)
+    res = db.find({"name": "bar"})
+
+    assert res != None
+    db.delete({"name": "bar"})
+    assert len(db.find_all()) == 0
+
+
+def test_composite(tmp_path):
+    db1 = MIDatasetDBBaseYaml(path=f"{tmp_path}/midatasets1.yaml")
+    db2 = MIDatasetDBBaseYaml(path=f"{tmp_path}/midatasets2.yaml")
+
+    for i in range(10):
+        m = MIDatasetModel(name=str(i), aws_s3_bucket="v", aws_s3_prefix="s")
+        if i % 2 == 0:
+            db1.create(m)
+        else:
+            db2.create(m)
+
+    cdb = CompositeDB([db1, db2])
+    assert len(cdb.find_all()) == 10
