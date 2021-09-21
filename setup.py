5cae2ad41912ffff721092780575449f54111af0
fk128 <fkanavati@gmail.com>
2021-09-21 11:57:49 +0100

    Bump version: 0.7.2 → 0.8.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as requirements_file:
 
 
 setup(name='midatasets',
-      version='0.7.2',
+      version='0.8.0',
       description='Medical Image Dataset tools',
       author='F. K.',
       keywords='medical ',
