18dec5bb99371c88f861e5469c1294d17273d1dd
fk128 <fkanavati@gmail.com>
2022-03-08 16:42:56 +0000

    Bump version: 0.13.0 → 0.14.0

--- midatasets/__init__.py
+++ midatasets/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.13.0"
+__version__ = "0.14.0"
 
 import os
 from typing import Optional, Dict, List
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("requirements_dev.txt") as requirements_file:
 
 setup(
     name="midatasets",
-    version="0.13.0",
+    version="0.14.0",
     description="Medical Image Dataset tools",
     author="F. K.",
     keywords="medical ",
