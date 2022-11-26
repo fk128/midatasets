75a5ac937313b6d6c598c12f2397e2283fd9568f
fk128 <fkanavati@gmail.com>
2022-11-26 11:46:18 +0000

    Add parallel

--- midatasets/processing.py
+++ midatasets/processing.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from typing import Union
+from typing import Union, List
 
+from joblib import Parallel, delayed
 from loguru import logger
 from midatasets.MIReader import MImage
 from midatasets.preprocessing import sitk_resample
@@ -8,22 +9,29 @@ from midatasets.utils import get_spacing_dirname
 import SimpleITK as sitk
 
 
-def resample_mimage(image: MImage,
-                    target_spacing: Union[float, int], overwrite: bool = False):
-    output_path = image.local_path.replace(image.resolution_dir, get_spacing_dirname(target_spacing))
-    prefix = image.prefix.replace(image.resolution_dir, get_spacing_dirname(target_spacing))
+def resample_mimage(
+    image: MImage, target_spacing: Union[float, int], overwrite: bool = False
+):
+    output_path = image.local_path.replace(
+        image.resolution_dir, get_spacing_dirname(target_spacing)
+    )
+    prefix = image.prefix.replace(
+        image.resolution_dir, get_spacing_dirname(target_spacing)
+    )
     if not overwrite and Path(output_path).exists():
-        return MImage(bucket=image.bucket, prefix=prefix, key=image.key, base_dir=image.base_dir, validate_key=False)
+        return MImage(
+            bucket=image.bucket,
+            prefix=prefix,
+            key=image.key,
+            base_dir=image.base_dir,
+            validate_key=False,
+        )
     sitk_image = sitk.ReadImage(image.local_path)
     interpolation = (
-        sitk.sitkLinear
-        if "image" in image.key
-        else sitk.sitkNearestNeighbor
+        sitk.sitkLinear if "image" in image.key else sitk.sitkNearestNeighbor
     )
     interpolation_str = (
-        "sitk.sitkLinear"
-        if "image" in image.key
-        else "sitk.sitkNearestNeighbor"
+        "sitk.sitkLinear" if "image" in image.key else "sitk.sitkNearestNeighbor"
     )
     logger.info(
         f"[{image.key}/{Path(output_path).name}] resampling from {sitk_image.GetSpacing()} "
@@ -34,4 +42,21 @@ def resample_mimage(image: MImage,
     )
     Path(output_path).parent.mkdir(parents=True, exist_ok=True)
     sitk.WriteImage(sitk_image, output_path)
-    return MImage(bucket=image.bucket, prefix=prefix, key=image.key, base_dir=image.base_dir, validate_key=False)
+    return MImage(
+        bucket=image.bucket,
+        prefix=prefix,
+        key=image.key,
+        base_dir=image.base_dir,
+        validate_key=False,
+    )
+
+
+def resample_mimage_parallel(
+    images: List[MImage],
+    target_spacing: Union[float, int],
+    overwrite: bool = False,
+    n_jobs: int = -1,
+):
+    Parallel(n_jobs=n_jobs)(
+        delayed(resample_mimage)(image, target_spacing, overwrite) for image in images
+    )
